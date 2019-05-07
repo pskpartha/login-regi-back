09b0634cdfabf27a759cc94524ba6693e53c7386
Partha Sarathi Kar <partha.kar@avionix.eu>
2019-05-08 00:27:47 +0300

    new things

--- server.js
+++ server.js
@@ -1,25 +1,41 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+var router = express.Router();
+var bodyParser = require('body-parser');
+var db = require('./db/db');
+// app.get('/', function(req, res) {
+//     res.sendFile(path.join(__dirname, 'index.html'));
+// });
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
+// parse application/json
+app.use(bodyParser.json());
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 
 app.listen(process.env.PORT || 4000, function(){
     console.log('Your node js server is running');
 });
 
-var router = express.Router();
+app.get('/', (req, res) => {
+    res.status(200).send({
+      success: 'true',
+      message: 'todos retrieved successfully',
+      todos: db
+    })
+  });
 
 // Home page route
-router.get('/wiki', function(req, res) {
-  res.send('Wiki home page');
-});
+app.get('/wiki', function(req, res, next) {
+    res.status(200)
+      .json({
+        status: 'success',
+        message: 'wiki new works!'
+      });
+  });
 
-// About page route
-router.get('/about', function(req, res) {
-  res.send('About this wiki');
-});
 
-module.exports = router;
+  
