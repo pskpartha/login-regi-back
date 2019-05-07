e052a1aa554ffbad33e659dc760df6e43090786f
Partha Sarathi Kar <partha.kar@avionix.eu>
2019-05-07 22:25:17 +0300

    Initial setup for jwt token

---
+++ server.js
@@ -0,0 +1,23 @@
+// import express from 'express';
+// import db from './db/db';
+// Set up the express app
+let express = require('express');
+let db = require('./db/db');
+
+const app = express();
+// get all todos
+app.get('/api/v1/todos', (req, res) => {
+  res.status(200).send({
+    success: 'true',
+    message: 'todos retrieved successfully',
+    todos: db
+  })
+});
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`)
+});
+
+// var express = require('express');
+
