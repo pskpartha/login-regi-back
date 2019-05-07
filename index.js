f43394b3eba7ace5344febd69bc41bd3a331c9b8
Partha Sarathi Kar <partha.kar@avionix.eu>
2019-05-07 23:19:36 +0300

    new things

--- index.js
+++ index.js
@@ -5,9 +5,8 @@ app.get('/', function (req, res) {
    res.send('Hello World');
 })
 
-var server = app.listen(process.env.PORT || 3000, function () {
-   var host = server.address().address
-   var port = server.address().port
-   
-   console.log("Example app listening at http://%s:%s", host, port)
-})
+app.set( 'port', ( process.env.PORT || 5000 ));
+
+app.listen( app.get( 'port' ), function() {
+    console.log( 'Node server is running on port ' + app.get( 'port' ));
+ });
