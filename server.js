1f9c71ea2509a22f0fdbf04b6144d865f71b9fd6
Partha Sarathi Kar <partha.kar@avionix.eu>
2019-05-08 00:04:45 +0300

    new things

--- server.js
+++ server.js
@@ -9,3 +9,17 @@ app.get('/', function(req, res) {
 app.listen(process.env.PORT || 4000, function(){
     console.log('Your node js server is running');
 });
+
+var router = express.Router();
+
+// Home page route
+router.get('/wiki', function(req, res) {
+  res.send('Wiki home page');
+});
+
+// About page route
+router.get('/about', function(req, res) {
+  res.send('About this wiki');
+});
+
+module.exports = router;
